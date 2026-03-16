cb0a1afdf578723e0b91371b7921e38e4e8935e9
chaali03 <zaidan.ali2411@gmail.com>
2026-03-16 21:01:52 +0700

    fix: auto detect xmrig location

--- start-mining-android.sh
+++ start-mining-android.sh
@@ -2,7 +2,16 @@
 # Script untuk langsung mining di Android
 # Wallet: 3B7VePx71mP8SnvmnGNSapkhratFY1iGct
 
-cd ~/xmrig/build
+# Cari lokasi xmrig
+if [ -d ~/xmrig/build ]; then
+    cd ~/xmrig/build
+elif [ -d ~/downloads/xmrig/build ]; then
+    cd ~/downloads/xmrig/build
+else
+    echo "ERROR: XMRig tidak ditemukan!"
+    echo "Install dulu dengan: pkg install -y git cmake libuv openssl && cd ~ && git clone https://github.com/xmrig/xmrig.git && cd xmrig && mkdir build && cd build && cmake .. -DWITH_HWLOC=OFF && make -j\$(nproc)"
+    exit 1
+fi
 
 echo "=== Memulai Mining ==="
 echo "Wallet: 3B7VePx71mP8SnvmnGNSapkhratFY1iGct"
